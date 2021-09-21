65e39608e077fc383c40939cf6f4498cc45aeb69
Caleb1991 <calebjwittman1991@gmail.com>
2021-09-21 10:35:48 -0600

    Create subsciption migration

---
+++ app/models/subscription.rb
@@ -0,0 +1,8 @@
+class Subscription < ApplicationRecord
+  validates_presence_of :title, :price, :status, :frequency, required: true
+  belongs_to :customer
+  has_many :teas
+
+  enum status: [:active, :paused, :cancelled]
+  enum frequency: [:monthly, :every_three_months, :every_six_months, :annually]
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_161659) do
+ActiveRecord::Schema.define(version: 2021_09_21_162541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,16 @@ ActiveRecord::Schema.define(version: 2021_09_21_161659) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.integer "status"
+    t.integer "frequency"
+    t.bigint "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+  end
+
+  add_foreign_key "subscriptions", "customers"
 end
