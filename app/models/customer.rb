de3f4934b30d8cac3b6157b86a51e4cca333a54b
Caleb1991 <calebjwittman1991@gmail.com>
2021-09-21 10:22:27 -0600

    Add relaitonship and validation tests for subscriptions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  validates_presence_of :first_name, :last_name, :email, :address
+  validates_presence_of :first_name, :last_name, :email, :address, required: true
   has_many :subscriptions, dependent: :destroy
   has_many :teas, through: :subscriptions
 end
